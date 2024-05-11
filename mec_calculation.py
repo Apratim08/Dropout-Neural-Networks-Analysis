98dfa12b016f9aea50bd73f9ef924afbf7f4db06
abiramisab <143840133+abiramisab@users.noreply.github.com>
2024-05-10 19:32:59 -0700

    Update mec_calculation.py

--- mec_calculation.py
+++ mec_calculation.py
@@ -1,26 +1,10 @@
 def MECwithP(p):
     input = (28**3) + 28
-    # print(input)
-
     bias_per_layer = (2048*p)
-
-    # layer 1: # of neurons: 2048*p because (0<p <1)
-    
     layer1 = min((2048*p)*28 + bias_per_layer, input)
-    # print(layer1)
-
-    # layer 2: # of neurons: 2048*p
     layer2 = min((2048*p)**2 + bias_per_layer, layer1)
-    # print(layer2)
-
     layer3 = min((2048*p)**2 + bias_per_layer, layer2)
-    # print(layer3)
-
-    #output layer: 2048*p*10
-
     out = min(2048*p*10 + 10, layer3)
-    # print(out)
-
     return input + layer1 + layer2 + layer3 + out
 
 
